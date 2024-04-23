7ed1fff804a45f5866a11e063d28f211467d5221
Subrat Katiyar <subratkatiyar50@gmail.com>
2024-04-23 23:21:39 +0530

    implemented cpu instructions.

--- src/cpu.c
+++ src/cpu.c
@@ -67,9 +67,49 @@ void execute_load_4(GameboyMemory *memory, GameboyRegisters *registers){
     uint8_t value = registers->a;
     printf("LOAD *HL A\n"); 
     write_memory(memory, registers->hl, value);
+    registers->hl -= 0x01;
     registers->pc += 0x01;
 }
 
+void execute_decrease_1(GameboyMemory *memory, GameboyRegisters *registers){
+    printf("DEC B\n");
+    uint8_t original_value = registers->b;
+    (registers->b)--;
+
+    if(registers->b == 0x00 ){
+        registers->f |= (1 << 7);  // Set zero flag (Z = 1)
+    }
+    else{
+        registers->f &= ~(1 << 7); // Clear zero flag (Z = 0)
+    }
+
+    // Set subtraction flag (N = 1) to indicate subtraction operation
+    registers->f |= (1 << 6);  // Set subtraction flag (N = 1)
+
+    // Set half-carry flag (H) if there was a borrow from bit 4 to bit 3
+    if ((original_value & 0x0F) == 0x00) {
+        registers->f |= (1 << 5);  // Set half-carry flag (H = 1)
+    } else {
+        registers->f &= ~(1 << 5); // Clear half-carry flag (H = 0)
+    }
+
+    registers->pc += 0x01;
+}
+
+void execute_conditional_jump_1(GameboyMemory *memory, GameboyRegisters *registers){
+    uint16_t valueLoc = registers->pc + 0x01;
+    uint16_t value = read_memory_16_le(memory, valueLoc);
+
+    printf("JR NZ 0x%04X\n", value); 
+
+
+    if((registers->f & (1<<7)) == 0x00){
+        registers->pc = value;
+    }
+    else{
+        registers->pc += 0x02;
+    }
+}
 
 void execute_instruction(GameboyMemory *memory, GameboyRegisters *registers, uint8_t opcode){
     switch(opcode){
@@ -93,6 +133,12 @@ void execute_instruction(GameboyMemory *memory, GameboyRegisters *registers, uin
             break;
         case 0x32:
             execute_load_4(memory, registers);
+            break;
+        case 0x05:
+            execute_decrease_1(memory, registers);
+            break;
+        case 0x20:
+            execute_conditional_jump_1(memory, registers);
             break;
         default:
             execute_unimplemented();
